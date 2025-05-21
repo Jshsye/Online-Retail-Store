94f0cbc98211b0fad44dc879ab9c4b09f95b5acd
Pavan V Rajoli <pavanrajoli25@gmail.com>
2025-05-21 13:00:25 +0530

    1_question.py

--- 1_question.py
+++ 1_question.py
@@ -5,7 +5,6 @@ file= pd.read_csv('OnlineRetail.csv', encoding='latin1')
 pd.set_option('display.max_columns', None)
 # print(file.head())
 
-# Add a new column called Revenue = Quantity × UnitPrice and calculate the total revenue.
 
 file['Revenue']= file['Quantity'] * file['UnitPrice']
 
