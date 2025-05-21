f0a5442f5142393c8def2aa00b41163efff20a75
Pavan V Rajoli <pavanrajoli25@gmail.com>
2025-05-21 13:02:18 +0530

    8_question.py

--- 8_question.py
+++ 8_question.py
@@ -5,19 +5,15 @@ import matplotlib.pyplot as plt
 file = pd.read_csv('OnlineRetail.csv', encoding='latin1')
 pd.set_option('display.max_columns', None)
 
-# Convert InvoiceDate to datetime
+
 file['InvoiceDate'] = pd.to_datetime(file['InvoiceDate'])
 
-# Extract hour from InvoiceDate
 file['Hour'] = file['InvoiceDate'].dt.hour
 
-# Count number of purchases by hour
 hourly_orders = file['Hour'].value_counts().sort_index()
 
-# Display the result
 print(hourly_orders)
 
-# Optional: Plot the result
 plt.figure(figsize=(10,6))
 plt.plot(hourly_orders.index, hourly_orders.values, marker='o', color='green')
 plt.title('Purchases by Hour of the Day')
